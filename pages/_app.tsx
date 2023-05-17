2d59020c0a22a84e27479e80809212665952ebb4
Rozmukhamedov Islom <workislom@gmail.comgit config --global user.name Rozmukhamedov>
2023-05-17 08:49:10 +0500

    Create project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import variables from "../styles/variables.module.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div color={variables.primaryColor}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
